f879325e420a46f8b8f106387dbd44bb62124a62
Ezra Jeonadab G. Del Rosario <ezrajeonadabg@gmail.com>
2026-03-11 11:24:11 +0800

    feat: added Neural Network model and BERT encoding for deep learning

--- classifiers.py
+++ classifiers.py
@@ -1,5 +1,26 @@
 import torch
+from torch import nn
 
-class NeuralNetwork:
-    def __init__(self):
-        pass
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size: int, hidden_layers: list, output_size: int): 
+        super().__init__()
+
+        layer = []
+
+        layer.append(nn.Flatten())
+
+        sizes = [input_size] + hidden_layers + [output_size]
+
+        for i in range(len(sizes) - 2):
+            layers = [
+                nn.Linear(sizes[i], sizes[i+1]),
+                nn.ReLU()
+            ]
+            layer.extend(layers)
+
+        layer.append(nn.Linear(sizes[-2], sizes[-1]))
+
+        self.model = nn.Sequential(*layer)
+        
+    def forward(self, x):
+        return self.model(x)
